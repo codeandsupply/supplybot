ded185bd9346b6d2ce7606854e1080427d41e45a
Nate Drake <nate.drake@firstinsight.com>
2016-10-08 18:10:13 -0400

    Fix build night finder and a little error handling

--- scripts/riddler_when_build_night.js
+++ scripts/riddler_when_build_night.js
@@ -2,12 +2,16 @@ _ = require('lodash/fp');
 var eventsUrl = process.env.EVENTS_URL;
 
 module.exports = function (robot) {
+  var now;
   var titleOf = function (e) { return e.title; };
   var dateOf = function (e) { return e.date; };
   var locationOf = function (e) { return e.location || "(location not set)"; };
   var dateSorted = _.sortBy(dateOf);
   var dateFilter = _.filter(function (event) { return (event.date !== null); });
-  var findBuildNight = _.find({ 'title': 'Build Night' });
+  var findBuildNight = _.find(function (e) {
+    return e.title === 'Build Night' && new Date(e.date) >= now;
+  });
+
   var ensureFindBuildNight = function (e) {
     if (findBuildNight(e) === undefined) {
       return "Build Night has not been scheduled.";
@@ -36,11 +40,19 @@ module.exports = function (robot) {
   var concatBuildNight = function (e) { return "Build Night is on " + formatDate(dateOf(e)) + ", " + locationOf(e); };
   var nextBuildNight = _.flow(dateFilter, dateSorted, ensureFindBuildNight);
 
-  //return robot.hear(/build night/i, function(msg) { // returns when/where of build night
-  //return msg.http(eventsUrl).get()(function(err, res, body) {
-  //    var json = JSON.parse(body);
-
-  //        return msg.send(nextBuildNight(json));
-  //});
-  //});
+  return robot.hear(/build night/i, function (msg) { // returns when/where of build night
+    if (!eventsUrl) {
+      msg.send('Please set the EVENTS_URL environment variable.');
+      return;
+    }
+    return msg.http(eventsUrl).get()(function (err, res, body) {
+      if (err) {
+        msg.send('Sorry, there was an error getting the event list.');
+        return;
+      }
+      now = new Date();
+      var json = JSON.parse(body);
+      return msg.send(nextBuildNight(json));
+    });
+  });
 };
