73050510595541c29e14a38def18ad85c36f513e
Justin Reese <justin.x.reese@gmail.com>
2016-10-12 18:44:43 +0000

    Update conversation_starter.js

--- scripts/conversation_starter.js
+++ scripts/conversation_starter.js
@@ -1,7 +1,7 @@
 module.exports = function(robot) {
-  return robot.respond(/quiet in here/i), function(msg) {
-    return msg.http("http://chatoms.com/chatom.json?Normal=1&Fun=2&Philosophy=3&Out+There=4&Love=5&Personal=7").get(), function(err, res, body){
-        return msg.send JSON.parse(body).text;
-    }
-  }
-}
+  return robot.respond(/chat/i, function(msg) {
+    return msg.http("http://chatoms.com/chatom.json?Normal=1&Fun=2&Philosophy=3&Out+There=4&Love=5&Personal=7").get()(function(err, res, body) {
+      return msg.send(JSON.parse(body).text);
+    });
+  });
+};
