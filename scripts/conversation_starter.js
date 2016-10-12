e4ddd81a740982ad5f4346ac7203dcdc0289309d
Justin Reese <justin.x.reese@gmail.com>
2016-10-12 15:18:30 -0400

    Fixes conversation starter

--- scripts/conversation_starter.js
+++ scripts/conversation_starter.js
@@ -1,7 +1,7 @@
 module.exports = function(robot) {
   return robot.hear(/quiet in here/i, function(msg) {
-    return msg.http("http://chatoms.com/chatom.json?Normal=1&Fun=2&Philosophy=3&Out+There=4&Love=5&Personal=7").get()(function(err, res, body) {
-      return msg.send(JSON.parse(body).text);
+    return msg.http("http://www.chatoms.com/chatom.json?Normal=1&Fun=2&Philosophy=3&Out+There=4&Love=5&Personal=7").get()(function(err, res, body) {
+      return msg.send("Maybe this will start a conversation? " + JSON.parse(body).text);
     });
   });
 };
