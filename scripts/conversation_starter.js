7891be6b142e53b4329f9e7bfdda22ed07bfe88f
Justin Reese <justin.x.reese@gmail.com>
2016-10-12 18:55:07 +0000

    Change conversation_starter.js trigger

--- scripts/conversation_starter.js
+++ scripts/conversation_starter.js
@@ -1,5 +1,5 @@
 module.exports = function(robot) {
-  return robot.respond(/chat/i, function(msg) {
+  return robot.hear(/quiet in here/i, function(msg) {
     return msg.http("http://chatoms.com/chatom.json?Normal=1&Fun=2&Philosophy=3&Out+There=4&Love=5&Personal=7").get()(function(err, res, body) {
       return msg.send(JSON.parse(body).text);
     });
