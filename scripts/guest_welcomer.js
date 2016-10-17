296b8d6cba10bf63148c4e279328983855c0d18b
Justin Reese <justin.x.reese@gmail.com>
2016-10-17 18:32:41 -0400

    Refinement of welcoming

--- scripts/guest_welcomer.js
+++ scripts/guest_welcomer.js
@@ -1,11 +1,11 @@
 var enterReplies;
 
-enterReplies = ['Hi', 'Welcome', 'Hello there', 'Hello, friend.'];
+enterReplies = ['Hi!', 'Welcome!', 'Hello there!', 'Hello, friend!'];
 
 module.exports = function(robot) {
   return robot.enter(function(res) {
-    res.send(res.random(enterReplies));
-    res.send('Be sure to familiarize yourself with our conduct policies located at http://www.codeandsupply.co/code-of-conduct/');
-    return res.send('Happy to have you here');
+    res.send('Someone new! '+res.random(enterReplies));
+    res.send('Be sure to familiarize yourself with our conduct policies, there should be a link in the header.');
+    return res.send('Happy to have you here!');
   });
 };
