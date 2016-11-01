c2d6f5641315bc8573e8b10caf98fb90daf93657
Colin Dean <git@cad.cx>
2016-10-31 20:57:39 -0400

    Puts one prompt per line for better readability

--- scripts/guest_welcomer.js
+++ scripts/guest_welcomer.js
@@ -1,8 +1,31 @@
 var enterReplies;
 
-enterReplies = ['Hi!', 'Welcome!', 'Hello there!', 'Hello, friend!'];
-welcomePrompts = ["What's your favorite color?", "Do you have a favorite programming language?",
-				"Do you like ketchup?", "What is your favorite car?" , "Where are you from?", "What's your favorite movie?", "What are your hobbies?", "What’s your hidden talent?", "Which animal would you choose to be?", "What is your favorite book?", "Which time period would you visit in history?", "What's your dream job?", "Where is your dream vacation spot?", "What is your favorite sport?", "Do you like to run?", "Do you like cheeseburgers?", "Have you ever been skydiving?", "Do you like coffee?"];
+enterReplies = [
+	'Hi!',
+	'Welcome!',
+	'Hello there!',
+	'Hello, friend!'
+];
+welcomePrompts = [
+    "What's your favorite color?",
+    "Do you have a favorite programming language?",
+    "Do you like ketchup?",
+    "What is your favorite car?",
+    "Where are you from?",
+    "What's your favorite movie?",
+    "What are your hobbies?",
+    "What’s your hidden talent?",
+    "Which animal would you choose to be?",
+    "What is your favorite book?",
+    "Which time period would you visit in history?",
+    "What's your dream job?",
+    "Where is your dream vacation spot?",
+    "What is your favorite sport?",
+    "Do you like to run?",
+    "Do you like cheeseburgers?",
+    "Have you ever been skydiving?",
+    "Do you like coffee?"
+];
 
 module.exports = function(robot) {
   return robot.enter(function(res) {
